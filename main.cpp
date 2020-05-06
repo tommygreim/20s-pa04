8f2707a034aada21d6d951b2cae7cae65779e32c
tommygreim <tommygreim1@gmail.com>
2020-05-06 02:44:34 -0500

    Cleaned up output. Added multiple tests. Added build.yaml

--- main.cpp
+++ main.cpp
@@ -9,11 +9,12 @@ int main() {
     //Assigns the number of elements per test case
     const int NUM_ELEMENTS = 10000000;
 
-
-    std::default_random_engine rng;
     //Set seed ensures the same numbers are generated every time
-    rng.seed(100);
+    std::default_random_engine rng(1000);
+    std::uniform_int_distribution<int> dist(0, NUM_ELEMENTS);
 
+
+    //      TEST CASE #1 (NUM ELEMENTS # Integers)
 
     std::vector<int> testCase1(NUM_ELEMENTS);// = {5,4,2343,351,126};
     //Inserts every number from 1 to num_elements into the vector
@@ -23,32 +24,44 @@ int main() {
     }
 
     //shuffle the contents of the vector
-    std::shuffle(testCase1.begin(), testCase1.end(), rng);
+    for(int i = 0; i < 10; i++) {
+        //Determines which element to find
+        int nthElement = dist(rng);
+        std::cout << "Test Case #" << i + 1 << ": Finding the " << nthElement << "th smallest element"<< std::endl;
 
-    std::chrono::steady_clock::time_point preSelect, postSelect;
-    std::chrono::duration<double> selectTime{};
+        std::shuffle(testCase1.begin(), testCase1.end(), rng);
 
+        std::chrono::steady_clock::time_point preSelect, postSelect;
+        std::chrono::duration<double> selectTime{};
 
-    //The Non-Trivial QuickSelect Selection Algorithm
-    std::vector<int> testCase1Q = testCase1;
+        //The Trivial Selection Algorithm
+        std::vector<int> testCase1T = testCase1;
 
-    preSelect = std::chrono::steady_clock::now();
-    int thirdSmallest = *quickSelect<std::vector<int>>::selectNthSmallest(testCase1Q, 19);
-    postSelect = std::chrono::steady_clock::now();
-    selectTime = postSelect - preSelect;
+        preSelect = std::chrono::steady_clock::now();
+        int foundSmallest = *trivialSelect<std::vector<int>>::selectNthSmallest(testCase1T, nthElement);
+        postSelect = std::chrono::steady_clock::now();
+        selectTime = postSelect - preSelect;
 
-    std::cout << std::endl << thirdSmallest << std::endl;
-    std::cout << "Time to find: " << selectTime.count() << " seconds." << std::endl;
+        std::cout << "\t==Trivial Implementation==" << std::endl;
+        std::cout << "\tFound: " << foundSmallest << std::endl;
+        std::cout << "\tTime to find: " << selectTime.count() << " seconds." << std::endl;
 
-    std::vector<int> testCase1T = testCase1;
+        std::cout << std::endl;
 
-    preSelect = std::chrono::steady_clock::now();
-    thirdSmallest = *trivialSelect<std::vector<int>>::selectNthSmallest(testCase1T, 19);
-    postSelect = std::chrono::steady_clock::now();
-    selectTime = postSelect - preSelect;
+        //The Non-Trivial QuickSelect Selection Algorithm
+        std::vector<int> testCase1Q = testCase1;
 
-    std::cout << std::endl << thirdSmallest << std::endl;
-    std::cout << "Time to find: " << selectTime.count() << " seconds." << std::endl;
+        preSelect = std::chrono::steady_clock::now();
+        foundSmallest = *quickSelect<std::vector<int>>::selectNthSmallest(testCase1Q, nthElement);
+        postSelect = std::chrono::steady_clock::now();
+        selectTime = postSelect - preSelect;
 
+        std::cout << "\t==Non-Trivial Implementation==" << std::endl;
+        std::cout << "\tFound: " << foundSmallest << std::endl;
+        std::cout << "\tTime to find: " << selectTime.count() << " seconds." << std::endl;
+
+        std::cout << std::endl << std::endl;
+
+    }
     return 0;
 }
